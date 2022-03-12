ad1c28d343a0293d3f5799a17a695862051c62a3
zchmielewska <zchmielewska@gmail.com>
2022-03-12 15:26:27 +0100

    require strong password

--- account/forms.py
+++ account/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import password_validation
 from django.contrib.auth.models import User
 
 from account.models import Profile
@@ -10,18 +11,27 @@ class LoginForm(forms.Form):
 
 
 class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput, min_length=8)
     password2 = forms.CharField(label="Repeat password", widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ("email", "first_name", "last_name")
 
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        try:
+            password_validation.validate_password(password, self.instance)
+        except forms.ValidationError as error:
+            self.add_error("password", error)
+        return password
+
     def clean_password2(self):
-        cd = self.cleaned_data
-        if cd["password"] != cd["password2"]:
+        password = self.cleaned_data["password"]
+        password2 = self.cleaned_data["password2"]
+        if password != password2:
             raise forms.ValidationError("Passwords don\'t match.")
-        return cd["password2"]
+        return password2
 
 
 class UserEditForm(forms.ModelForm):
