e8d9b5b0e10befab6bd9b0d0a82a2e4ce92212ed
zchmielewska <zchmielewska@gmail.com>
2022-03-23 20:04:15 +0100

    add postgres db

--- actudoc/settings.py
+++ actudoc/settings.py
@@ -84,27 +84,25 @@ WSGI_APPLICATION = 'actudoc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if DEBUG:
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'actudoc',
+            'USER': os.getenv("DB_USER"),
+            'PASSWORD': os.getenv("DB_PASSWORD"),
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
-
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'actudoc',
-#             'USER': os.getenv("ADMIN_NAME"),
-#             'PASSWORD': os.getenv("ADMIN_NAME"),
-#             'HOST': 'localhost',
-#             'PORT': '',
-#         }
-#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
