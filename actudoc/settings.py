4be8ff639e2e0ca8cf65e8d794db69af46109940
zchmielewska <zchmielewska@gmail.com>
2022-03-23 08:33:46 +0100

    hotfix settings

--- actudoc/settings.py
+++ actudoc/settings.py
@@ -86,6 +86,14 @@ WSGI_APPLICATION = 'actudoc.wsgi.application'
 
 # if DEBUG:
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
 # else:
 #     DATABASES = {
 #         'default': {
