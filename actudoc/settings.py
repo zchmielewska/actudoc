499aee42158a9ae8c8cdd766ce8c693bff4e6c1b
zchmielewska <zchmielewska@gmail.com>
2022-03-26 18:54:39 +0100

    fix smtp

--- actudoc/settings.py
+++ actudoc/settings.py
@@ -161,11 +161,13 @@ FIXTURE_DIRS = (
 )
 
 # E-mail backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_USE_TLS = (os.getenv("EMAIL_USE_TLS") == "True")
+DEFAULT_FROM_EMAIL=os.getenv("EMAIL_HOST_USER")
 
 # Authentication via e-mail
 AUTHENTICATION_BACKENDS = [
--- actudoc/urls.py
+++ actudoc/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
     path('account/', include('account.urls', namespace='account')),
     path('', include('document.urls')),
 ]
